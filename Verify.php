8790d0f314f05408af72d9f05e87ac30cf0ec9b2
aitx85 <31533309+aitx85@users.noreply.github.com>
2017-09-01 16:12:10 +0700

    Create Verify.php

---
+++ Verify.php
@@ -0,0 +1,11 @@
+<?php
+$access_token = ' 7eowgZxpltnVD2SrM/VxvNyOYmZ5FiJD1dghIWJoS4eLp48oWwaG48Wd76uZsEQN1GNU8NvJ109COxatExDibCHstzjdLvdPCJo1x2i1ok4io60b9MQVa7a38HxqwmzNKKZNFbrzWfNj3R9m+GnUXAdB04t89/1O/w1cDnyilFU= ';
+$url = 'https://api.line.me/v1/oauth/verify';
+$headers = array('Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
