7c7e87257f7412f24460000174669b184ca86c00
aitx85 <31533309+aitx85@users.noreply.github.com>
2017-09-01 16:33:20 +0700

    Update Verify.php

--- Verify.php
+++ Verify.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = '7eowgZxpltnVD2SrM/VxvNyOYmZ5FiJD1dghIWJoS4eLp48oWwaG48Wd76uZsEQN1GNU8NvJ109COxatExDibCHstzjdLvdPCJo1x2i1ok4io60b9MQVa7a38HxqwmzNKKZNFbrzWfNj3R9m+GnUXAdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v1/oauth/Verify';
+$url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' ,$access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
